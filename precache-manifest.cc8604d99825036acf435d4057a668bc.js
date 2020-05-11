a061a065a4d45982e57fb3654ca0810254357990
Jaisy <abdurrahmanjaisy@mail.ugm.ac.id>
2020-05-11 19:02:11 +0700

    Updates

---
+++ precache-manifest.cc8604d99825036acf435d4057a668bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e473599c19ef5594b9447753283686a",
+    "url": "/keluarrBerapa/index.html"
+  },
+  {
+    "revision": "6c56f50d32943d943eaa",
+    "url": "/keluarrBerapa/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "fdfc340021bd113ef617",
+    "url": "/keluarrBerapa/static/css/main.1fc43244.chunk.css"
+  },
+  {
+    "revision": "6c56f50d32943d943eaa",
+    "url": "/keluarrBerapa/static/js/2.1ce63dfa.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/keluarrBerapa/static/js/2.1ce63dfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdfc340021bd113ef617",
+    "url": "/keluarrBerapa/static/js/main.9b993278.chunk.js"
+  },
+  {
+    "revision": "d82fef8c2533ed5f0129",
+    "url": "/keluarrBerapa/static/js/runtime-main.3962861e.js"
+  }
+]);
