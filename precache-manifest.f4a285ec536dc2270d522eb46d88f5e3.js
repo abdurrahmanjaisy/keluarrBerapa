6470fc62c8e085bfeb1fdcd6dd502804f3721fda
Jaisy <abdurrahmanjaisy@mail.ugm.ac.id>
2020-05-11 20:49:37 +0700

    Updates

--- precache-manifest.f4a285ec536dc2270d522eb46d88f5e3.js
+++ precache-manifest.f4a285ec536dc2270d522eb46d88f5e3.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9e473599c19ef5594b9447753283686a",
+    "revision": "7b75433bc0ccf251b61175b3ca95a445",
     "url": "/keluarrBerapa/index.html"
   },
   {
-    "revision": "6c56f50d32943d943eaa",
+    "revision": "5b64833c7f03dd9f80ab",
     "url": "/keluarrBerapa/static/css/2.74189dbb.chunk.css"
   },
   {
-    "revision": "fdfc340021bd113ef617",
+    "revision": "98081740b6e3193d3cb3",
     "url": "/keluarrBerapa/static/css/main.1fc43244.chunk.css"
   },
   {
-    "revision": "6c56f50d32943d943eaa",
-    "url": "/keluarrBerapa/static/js/2.1ce63dfa.chunk.js"
+    "revision": "5b64833c7f03dd9f80ab",
+    "url": "/keluarrBerapa/static/js/2.36a556cf.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/keluarrBerapa/static/js/2.1ce63dfa.chunk.js.LICENSE.txt"
+    "url": "/keluarrBerapa/static/js/2.36a556cf.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fdfc340021bd113ef617",
-    "url": "/keluarrBerapa/static/js/main.9b993278.chunk.js"
+    "revision": "98081740b6e3193d3cb3",
+    "url": "/keluarrBerapa/static/js/main.23b14ab0.chunk.js"
   },
   {
     "revision": "d82fef8c2533ed5f0129",
